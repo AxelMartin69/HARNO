eb911d13565031bf660b69065118c065e6e88829
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-05-06 12:40:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="et">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/882cac8f18.js" crossorigin="anonymous"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    
+    
+    <link rel="stylesheet" href="css/style.css">
+    <title>Henno</title>
+</head>
+
+<body>
+    <div class='navbar'>
+        <ul>
+            <li><a><img src='' alt='EMLiit logo'></a></li>
+            <li><a><img src='' alt='HARNO logo'></a></li>
+            <li><a><img src='' alt='HTM logo'></a></li>
+        </ul>
+        <a><img src='' alt='SF logod'></a>
+        <h3><a rel='index.php'>EST</a>/<a rel='rus.php'>RUS</a></h3>
+    </div>
+    <div class='row-1'>
+        <div class='text'>
+            <h2>Oled hädas kvalifitseeritud tööjõu leidmisega ja järel-kasvu kindlustamisega?</h1>
+            <p>Uus profiõppe pilootmudel pakub lahendust, mis kasvatab kutseoskustega noori töötajaid, kes vastavad just Sinu ettevõtte vajadustele.</p>
+            <p>Profiõpe viib kokku kutsekooli, metalliettevõtte ja erialast tööd otsiva noore, kes on juba omandanud keskhariduse. Kolmepoolse kokkuleppe alusel toimub kutseõpe paralleelselt töösuhte alustamisega ettevõttes.</p>
+            <p>See tagab kiirema, kvaliteetsema ja täpselt ettevõtte vajadustele vastava uue meistri kujunemise - see ongi profiõpe.</p>
+        </div>
+        <div class='pic'></div>
+    </div>
+    <div class='row-2'>
+        <img src='' alt='VOCO logo'>
+        <img src='' alt='IdaViru logo'>
+        <img src='' alt='Viljadi kutsekas logo'>
+        <img src='' alt='Pärnu kutsekas logo'>
+        <img src='' alt='Rakvere kutsekas logo'>
+        <img src='' alt='Võru kutsekas logo'>
+        <img src='' alt='Lasnamäe kutsekas logo'>
+    </div>
+    <div class='row-3'><p>Koostöös metallierialasid õpetavate kutsekoolidega piloteerime töökohapõhist profiõpet kolmel erialal. Palun anna meile oma huvist ja võimalikest eelistustest märku - milliste erialade proffide järelkasvu Sinu ettevõte täna ja tulevikus kõige olulisemaks peab?</p></div>
+    <div class='row-4'>
+        <div class='item'>
+            <img src=<?php echo "''"; ?> alt=<?php echo "'CNC'"; ?>>
+            <label>
+                <input type="checkbox" class="filled-in" />
+                <span><?php echo 'CNC operaator'; ?></span>
+            </label>
+            <code>CNC operaatori profiõpe sisaldab metallilõikepinkide programmeerimist ja ka täpsust nõudvaid käelisi oskusi. Õppe käigus on võimalik spetsialiseeruda CNC treipingi või CNC freespingi operaatoriks. Õppe aluseks on CNC metallilõikepingi operaatori õppekava.</code>
+        </div>
+        <div class='item'>
+            <img src=<?php echo "''"; ?> alt=<?php echo "'Koostelukksepp'"; ?>>
+            <label>
+                <input type="checkbox" class="filled-in" />
+                <span><?php echo 'Koostelukksepp'; ?></span>
+            </label>
+            <code>Koostelukksepa tööülesanded on toorikute ettevalmistamine, detailide mõõtmine, märkimine, töötlemine, puhastamine ning koostude koostamine ja reguleerimine. See eeldab jooniste lugemise oskust, masinaehitus-tehnoloogia ja kasutatavate materjalide tundmist.</code>
+        </div>
+        <div class='item'>
+            <img src=<?php echo "''"; ?> alt=<?php echo "'Keevitaja'"; ?>>
+            <label>
+                <input type="checkbox" class="filled-in" />
+                <span><?php echo 'Keevitaja'; ?></span>
+            </label>
+            <code>Keevitaja valmistab detaile ja kooste, kasutades erinevaid keevitusseadmeid, lähtudes tööjoonisest, töötlemistehnoloogiatest, tööprotsessidest, töövahenditest ja töödeldavatest materjalidest.</code>
+        
+        </div>
+        
+    </div>
+    <div class='row-5'>
+        <h2>Või hoopis mõni muu eriala</h2>
+        <div class="input-field col s6">
+          <input id="muu" type="text" class="validate">
+          <label for="muu">Kirjuta siia</label>
+        </div>
+    </div>
+    
+    <div class='row-6'>
+        <div class='text'>
+            <h1>Kui hästi profiõpe Sinu ettevõttele sobida võiks?</h1>
+            <p>Palun jaga meiega lisainfot oma ettevõtte
+                tausta ja vajaduste kohta, et saaksime
+                hinnata uue loodava profiõppe mudeli
+                sobivust Sinu ettevõttesse uute
+                professionaalsete töötajate koolitamiseks.</p>
+        </div>
+        <div class='pic'></div>
+    </div>
+    
+    <div class='row-7'>
+        <div class="input-field col s6">
+          <input placeholder="ettevõtte nimi" id="1" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Kes tegeleb Sinu ettevõttes uute inimeste, sh praktikantide vastuvõtmisega?" id="2" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Milline on Sinu ettevõtte võimekus ja kogemus praktikantide personaalsel juhendamisel?" id="3" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Kas Sinu ettevõte on valmis lepinguga tööle võtma noore, kes samal ajal õpib kutsekoolis vajalikku eriala?" id="4" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Kas Sinu ettevõte oleks huvitatud osalema 2022/23 õppeaasta profiõppe pilootprogrammis?" id="5" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Mitu noort on Sinu ettevõte korraga võimeline ja valmis vastu võtma?" id="6" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Tekkis küsimusi? Tahaks midagi täpsustada? Kirjuta siia!" id="7" type="text" class="validate">
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Jäta meile oma e-mail kui soovid, et võtaksime Sinuga personaalselt ühendust!" id="8" type="text" class="validate">
+        </div>
+        
+        
+    </div>
+    
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+
+</html>
