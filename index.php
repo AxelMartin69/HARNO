3a0a259c3c5079dbf99da8b82d3d2975cad9d673
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-05-06 14:53:53 +0300

    sum styling done

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 	<link rel="stylesheet" href="css/style.css">
 	<title>Henno</title>
 </head>
-
-<body>
+<!-- Logod -->
+<body class="grey lighten-5">
 	<div class="container">
 		<div class="row">
 			<a href='#'>
@@ -58,7 +58,7 @@
 				<br>
 				<br>
 				<label>
-					<input type="checkbox" class="filled-in" />
+					<input type="checkbox" class="filled-in checkbox-blue-grey" />
 					<span class="black-tetx"><?php echo 'CNC operaator'; ?></span>
 				</label>
 				<br>
@@ -92,15 +92,15 @@
 			</div>
 
 		</div>
-		<div class='row-5'>
-			<h5>Või hoopis mõni muu eriala?</h5>
+		<div class='row'>
+			<h5 class="col s6">Või hoopis mõni muu eriala?</h5>
 			<div class="input-field col s6">
 				<input id="muu" type="text" class="validate">
 				<label for="muu">Kirjuta siia</label>
 			</div>
 		</div>
 
-		<div class='row-6'>
+		<div class='row'>
 			<div class='text'>
 				<h1>Kui hästi profiõpe Sinu ettevõttele sobida võiks?</h1>
 				<p>Palun jaga meiega lisainfot oma ettevõtte
@@ -111,34 +111,54 @@
 			</div>
 			<div class='pic'></div>
 		</div>
+<!-- input query -->
+		<div class='row'>
+			<div class="input-field">
+				<input id="1" type="text" class="validate">
+				<label for="1">Ettevõtte nimi</label>
+			</div>
+			<div class="input-field">
+				<input id="2" type="text" class="validate">
+				<label for="2">Kes tegeleb Sinu ettevõttes uute inimeste, sh praktikantide vastuvõtmisega?</label>
+			</div>
+			<div class="input-field">
+				<input id="3" type="text" class="validate">
+				<label for="3">Milline on Sinu ettevõtte võimekus ja kogemus praktikantide personaalsel juhendamisel?</label>
+			</div>
+			<div class="input-field">
+				<input id="4" type="text" class="validate">
+				<label for="4">Kas Sinu ettevõte on valmis lepinguga tööle võtma noore, kes samal ajal õpib kutsekoolis vajalikku eriala</label>
+			</div>
+			<div class="input-field">
+				<input id="5" type="text" class="validate">
+				<label for="5">Kas Sinu ettevõte oleks huvitatud osalema 2022/23 õppeaasta profiõppe pilootprogrammis?</label>
+			</div>
+			<div class="input-field">
+				<input id="6" type="text" class="validate">
+				<label for="6">Mitu noort on Sinu ettevõte korraga võimeline ja valmis vastu võtma?</label>
+			</div>
+			<div class="input-field">
+				<input id="7" type="text" class="validate">
+				<label for="7">Tekkis küsimusi? Tahaks midagi täpsustada? Kirjuta siia!</label>
+			</div>
+			<div class="input-field">
+				<input id="8" type="text" class="validate">
+				<label for="8">Jäta meile oma e-mail kui soovid, et võtaksime Sinuga personaalselt ühendust!</label>
+			</div>
+		</div>
 
-		<div class='row-7'>
-			<div class="input-field col s6">
-				<input placeholder="ettevõtte nimi" id="1" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Kes tegeleb Sinu ettevõttes uute inimeste, sh praktikantide vastuvõtmisega?" id="2" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Milline on Sinu ettevõtte võimekus ja kogemus praktikantide personaalsel juhendamisel?" id="3" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Kas Sinu ettevõte on valmis lepinguga tööle võtma noore, kes samal ajal õpib kutsekoolis vajalikku eriala?" id="4" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Kas Sinu ettevõte oleks huvitatud osalema 2022/23 õppeaasta profiõppe pilootprogrammis?" id="5" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Mitu noort on Sinu ettevõte korraga võimeline ja valmis vastu võtma?" id="6" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Tekkis küsimusi? Tahaks midagi täpsustada? Kirjuta siia!" id="7" type="text" class="validate">
-			</div>
-			<div class="input-field col s6">
-				<input placeholder="Jäta meile oma e-mail kui soovid, et võtaksime Sinuga personaalselt ühendust!" id="8" type="text" class="validate">
-			</div>
+		<div class='row'>
+			<div class='text'>
+				<p>Aitäh lisainfot jagamast – antud küsimustikule vastamine ei kohusta Sind ega Sinu ettevõtet veel millekski.
+					Küll aga saavad kiiremad ja suurema motivatsiooniga ettevõtted teiste ees olulise eelise – just nende
+					hulgast valitakse välja esimesed profiõppe pilootprogrammis osalejad. Huvilistega võtame täiendava info
+					ja tingimuste läbirääkimisteks eraldi ühendust.</p>
+		</div>
 
-
+		<div class='row'>
+			<div class='text'>
+				<p></p>
+			</div>
 		</div>
 	</div>
 
