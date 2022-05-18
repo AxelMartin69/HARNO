30fcae914332542ca8a56651f79d0e8c0a7fe5c2
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-05-18 10:06:10 +0300

    sum changes

--- index.php
+++ index.php
@@ -15,36 +15,7 @@
 </head>
 
 <?php
-
-// DATABASE
-
-function database($yks, $kaks, $kolm, $neli, $viis, $kuus, $seitse, $kaheksa) {
-	$servername = "localhost";
-	$username = "htv3";
-	$password = "8dV&%lVZ_3DH";
-	$dbname = "htv3_HARNO";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname)	;
-	// Check connection
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-	}
-
-	$sql = "INSERT INTO ettevotte (ettevotte_nimi, vastuvotja, personaal_juhtimine, opib, pilootprogramm, mitunoort, kysimus, 'e-mail')
-	VALUES ($yks, $kaks, $kolm, $neli, $viis, $kuus, $seitse, $kaheksa)";
-
-	if ($conn->query($sql) === TRUE) {
-	  echo "Andmed edukalt sisestatud!";
-	} else {
-	  echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-
-	$conn->close();
-};
-
-
-	
+	require 'db.php';
 ?>
 
 <!-- Logod -->
